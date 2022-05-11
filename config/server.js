54de08f6fd655ab133c184a97b0511541b40876c
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2022-05-11 10:34:56 +0100

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("https://joshportfolio-cms.herokuapp.com/"),
-  proxy: true,
   app: {
-    keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
+    keys: env.array('APP_KEYS'),
   },
 });
