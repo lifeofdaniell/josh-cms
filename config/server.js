b316b44e66fd56b0cb140b405afed0ed25439357
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2022-05-11 09:21:46 +0100

    testKey

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("https://joshportfolio-cms.herokuapp.com/"),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
   },
 });
