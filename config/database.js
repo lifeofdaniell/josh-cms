fb845a31a17eb633186915312579f3c3f34c98f3
Ayodeji Alarape <ayodeji775@gmail.com>
2023-01-28 16:13:52 +0100

    update production/db env structure | update package to ryn build with start

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5931),
-      database: env('PGDATABASE', 'josh-cms'),
-      user: env('PGUSER', 'root'),
-      password: env('PGPASSWORD', 'deji'),
-      ssl: env.bool(true),
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('PGHOST', '127.0.0.1'),
+//       port: env.int('PGPORT', 5931),
+//       database: env('PGDATABASE', 'josh-cms'),
+//       user: env('PGUSER', 'root'),
+//       password: env('PGPASSWORD', 'deji'),
+//       ssl: env.bool(true),
+//     },
+//   },
+// });
