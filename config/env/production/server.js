2fbb7b93150b508778296912ee9233a80231cef3
Ayodeji Alarape <ayodeji775@gmail.com>
2023-01-28 17:09:28 +0100

    update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "https://josh-cms-production.up.railway.app")
+  url: env("PUBLIC_URL", "https://josh-cms-production.up.railway.app"),
+    app: {
+    keys: env.array('APP_KEYS'),
+    
+  },
 });
